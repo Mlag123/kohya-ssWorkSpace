0a245236adb95a009fcb06030016f72a7c3c3fff
Mlag123 <disw@list.ru>
2025-09-28 02:26:27 +0700

    crop resolution from save proportions

--- src/Engine/Logic.java
+++ src/Engine/Logic.java
@@ -3,6 +3,9 @@ package Engine;
 import logging.Logging;
 import logging.logs;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -32,28 +35,64 @@ public class Logic {
 
 
     public void Rename(String rename_text) {
-        String rename = rename_text;
         File input = new File("./run/input");
-        // File output = new File("./run/output"); fixme 2462462462462462462462462
         arrFiles = new ArrayList<>();
-        for (File file : input.listFiles()
-        )
-            arrFiles.add(file);
-        {
-            sizeFiles = arrFiles;
-
-        }
-        for (int i = 0; i < arrFiles.size(); i++) {
-            a = i;
-            File file = arrFiles.get(i);
-            if (i == 0) {
-                File renameFile = new File("./run/output" + "/" + rename + ".png");
-                file.renameTo(renameFile);
+        for (File file : input.listFiles()) {
+            if (file.isFile()) {
+                arrFiles.add(file);
             }
-            File renameFile = new File("./run/output" + "/" + rename + i + ".png");
-            //   System.out.println(a);
-            if (file.renameTo(renameFile)) ;
-            // Thread.sleep(600);
+        }
+        sizeFiles = arrFiles;
+
+        for (int i = 0; i < arrFiles.size(); i++) {
+            File file = arrFiles.get(i);
+
+            // имя выходного файла
+            String newName = (i == 0) ? rename_text + ".png" : rename_text + i + ".png";
+            File outputFile = new File("./run/output/" + newName);
+
+            try {
+                String fileName = file.getName().toLowerCase();
+
+                if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".png")) {
+                    BufferedImage original = ImageIO.read(file);
+                    if (original != null) {
+                        // масштабирование с сохранением пропорций
+                        int targetSize = 512;
+                        double scale = Math.min(
+                                (double) targetSize / original.getWidth(),
+                                (double) targetSize / original.getHeight()
+                        );
+                        int newW = (int) (original.getWidth() * scale);
+                        int newH = (int) (original.getHeight() * scale);
+
+                        Image scaled = original.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+
+                        // создаём пустую 512x512 картинку
+                        BufferedImage resized = new BufferedImage(targetSize, targetSize, BufferedImage.TYPE_INT_ARGB);
+                        Graphics2D g2d = resized.createGraphics();
+
+                        // фон (чёрный)
+                        g2d.setColor(Color.BLACK);
+                        g2d.fillRect(0, 0, targetSize, targetSize);
+
+                        // рисуем картинку по центру
+                        int x = (targetSize - newW) / 2;
+                        int y = (targetSize - newH) / 2;
+                        g2d.drawImage(scaled, x, y, null);
+                        g2d.dispose();
+
+                        // сохраняем в PNG
+                        ImageIO.write(resized, "png", outputFile);
+                    }
+                } else {
+                    Logging.log( "Пропущен файл: " + file.getName());
+                }
+
+            } catch (IOException e) {
+                Logging.log(logs.error, "Ошибка при обработке файла: " + file.getName());
+                e.printStackTrace();
+            }
         }
     }
 
